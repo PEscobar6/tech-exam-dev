a55d1f4656cc1b3695310b1a220b160bb0bd09d2
Pablo Escobar <pablo.escobar@autofact.com.co>
2021-11-29 01:24:18 -0500

    HOTF-01 - Se cambia el puerto en donde corre el servidor debido a que la app no funcionaba

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const port = 3001;
+const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("ping");
 });
 
 app.listen(port, () => {
-    console.log('Server listening at port 3001');
+    console.log('Server listening at port 3000');
 })
